ac441ab2c7826d8407ac0024fbc0c435e496e9e2
Travis Nickels <nickels.travis@gmail.com>
2025-12-11 22:49:49 -0800

    Add default layout

---
+++ frontend/app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen flex flex-col bg-surface text-text">
+    <HeaderBar />
+
+    <main class="flex-1 container mx-auto px-6 py-12">
+      <slot />
+    </main>
+
+    <FooterBar />
+  </div>
+</template>
